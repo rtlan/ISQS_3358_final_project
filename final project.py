ee0f3c387c1469ad49793d216156a6c99f19d05d
rtlan <rlance921@gmail.com>
2026-03-24 13:41:33 -0500

    Rename requests import to r
    Renamed the 'requests' import to 'r' for brevity.

--- final project.py
+++ final project.py
@@ -1,5 +1,12 @@
-import requests
+import requests as r
 from bs4 import BeautifulSoup
 
 #URL
 url = https://www.pro-football-reference.com/
+
+# Fetching page
+res = r.get(url)
+res.raise_for_status() #if request fails
+
+#parsing
+soup = BeautifulSoup(res.text)
